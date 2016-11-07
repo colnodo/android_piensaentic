4bd65d27e13e69c3771ee3538b7f161b0760605c
Daniel Trujillo <DanielTrujillo>
2016-11-07 16:12:09 -0500

    =Changed toolbar for imageView in home

--- app/src/main/java/org/apc/colnodo/piensaentic/Home.java
+++ app/src/main/java/org/apc/colnodo/piensaentic/Home.java
@@ -1,17 +1,14 @@
 package org.apc.colnodo.piensaentic;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import org.apc.colnodo.piensaentic.GenericActivityPager.ActivityManager;
 import org.apc.colnodo.piensaentic.IndexManagement.ActivitiesIndex;
@@ -24,11 +21,12 @@ import java.util.List;
  * Created by apple on 11/5/16.
  */
 
-public class Home extends AppCompatActivity implements RightMenuFragment.OnOptionRightMenuClicked{
+public class Home extends AppCompatActivity implements View.OnClickListener, RightMenuFragment.OnOptionRightMenuClicked{
 
 
-    private Toolbar mToolbar;
     private DrawerLayout mRightMenu;
+    private ImageView mIbMenu;
+    private LinearLayout mLyMenuContainer;
     private ActivitiesIndex mIndex = new ActivitiesIndex();
     private List<String> activitiesList = new ArrayList();
 
@@ -38,13 +36,13 @@ public class Home extends AppCompatActivity implements RightMenuFragment.OnOptio
         super.onCreate(savedInstanceState);
         this.setTheme(R.style.AppTheme);
         setContentView(R.layout.home);
+        mIbMenu = (ImageView)findViewById(R.id.iv_hamburguesa);
+        mLyMenuContainer = (LinearLayout)findViewById(R.id.ly_right_menu_container);
+        mLyMenuContainer.bringToFront();
+        mIbMenu.bringToFront();
+        mIbMenu.setOnClickListener(this);
 
         mRightMenu = (DrawerLayout)findViewById(R.id.drawer_layout);
-
-        mToolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
-        ActionBar bar = getSupportActionBar();
-        bar.setDisplayShowTitleEnabled(false);
 
         mIndex.setActivities(this);
         startActivity(getNextActivityIndex());
@@ -61,13 +59,6 @@ public class Home extends AppCompatActivity implements RightMenuFragment.OnOptio
         mRightMenu.closeDrawer(GravityCompat.END);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.general_menu, menu);
-        return true;
-    }
-
     private int getNextActivityIndex(){
         return 0;
     }
@@ -77,31 +68,24 @@ public class Home extends AppCompatActivity implements RightMenuFragment.OnOptio
         ActivityManager actualFragment = new ActivityManager();
         actualFragment.setArguments(activityActual.mFragments, activityActual.mActivity_name,
                 activityActual.mBackground_id, activityActual.mPager_indicator_id);
-
-        FrameLayout frame = (FrameLayout)findViewById(R.id.ly_content_home);
         getSupportFragmentManager().beginTransaction().replace(R.id.ly_content_home, actualFragment).commit();
     }
 
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.action_settings:
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.iv_hamburguesa:
                 if (!mRightMenu.isDrawerOpen(GravityCompat.END)) {
                     mRightMenu.openDrawer(GravityCompat.END);
                 } else
                     mRightMenu.closeDrawer(GravityCompat.END);
-                return true;
+                break;
             default:
                 break;
+
         }
 
-        //noinspection SimplifiableIfStatement
-
-        return super.onOptionsItemSelected(item);
     }
-
 }
--- app/src/main/java/org/apc/colnodo/piensaentic/IndexManagement/ActivitiesIndex.java
+++ app/src/main/java/org/apc/colnodo/piensaentic/IndexManagement/ActivitiesIndex.java
@@ -66,29 +66,6 @@ public class ActivitiesIndex {
             actual.mFragments = getFragments(mActivitiesFragments.get(i));
             mIndex.add(actual);
         }
-
-//        ArrayList<Fragment> list = new ArrayList<>();
-//        list.add(BlankFragment.newInstance("Primero", "Primero"));
-//        list.add(BlankFragment.newInstance("Segundo", "Segundo"));
-//        list.add(BlankFragment.newInstance("Tercero", "Tercero"));
-//
-//        ArrayList<Fragment> list2 = new ArrayList<>();
-//        list2.add(BlankFragment.newInstance("Primero2", "Primero"));
-//        list2.add(BlankFragment.newInstance("Segundo2", "Segundo"));
-//        list2.add(BlankFragment.newInstance("Tercero2", "Tercero"));
-//        ArrayList<Fragment> list3 = new ArrayList<>();
-//
-//        list3.add(BlankFragment.newInstance("Primero3", "Primero"));
-//        list3.add(BlankFragment.newInstance("Segundo3", "Segundo"));
-//        list3.add(BlankFragment.newInstance("Tercero3", "Tercero"));
-//
-//        Activity act1 = new Activity(list, "Act1",getResourceId("act1_fondo"),getResourceId("select_pager_indicator_blue"));
-//        Activity act2 = new Activity(list2, "Act2",getResourceId("intro_fondo"),getResourceId("select_pager_indicator_red"));
-//        Activity act3 = new Activity(list3, "Act3",getResourceId("act1_fondo"),getResourceId("select_pager_indicator_red"));
-//
-//        mIndex.add(act1);
-//        mIndex.add(act2);
-//        mIndex.add(act3);
     }
 
     private ArrayList<Fragment> getFragments(String activitiesFragments) {
