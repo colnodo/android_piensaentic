11a60dc3086696536f1cb2010ad9e17e0ca10601
Daniel Trujillo <DanielTrujillo>
2016-11-21 15:20:24 -0500

    =Added youtube video to activity 5, fixed view some bugs

--- app/src/main/java/org/apc/colnodo/piensaentic/Activities/ActivityFiveEncryption/One.java
+++ app/src/main/java/org/apc/colnodo/piensaentic/Activities/ActivityFiveEncryption/One.java
@@ -7,16 +7,27 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 import org.apc.colnodo.piensaentic.IndexManagement.FragmentBookInterface;
 import org.apc.colnodo.piensaentic.R;
+import org.apc.colnodo.piensaentic.Utils.LocalConstants;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -43,8 +54,9 @@ public class One extends Fragment {
     List<TextView> mTvValues;
     View mView;
     FragmentBookInterface mFragmentInterface;
-
-
+    private YouTubePlayerSupportFragment youTubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+    private Button mPlayButton;
 
     public One(){}
 
@@ -64,6 +76,32 @@ public class One extends Fragment {
                              Bundle savedInstanceState) {
         mTvValues = new ArrayList<>();
         mView = inflater.inflate(R.layout.activity_five_one, container, false);
+        LinearLayout lyYoutube = (LinearLayout)mView.findViewById(R.id.ly_youtube_video_frame);
+        lyYoutube.removeAllViews();
+        youTubePlayerView = YouTubePlayerSupportFragment.newInstance();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.ly_youtube_video_frame, youTubePlayerView).commit();
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo(LocalConstants.YOUTUBE_VIDEO_ID);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        mPlayButton = (Button) mView.findViewById(R.id.button);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                youTubePlayerView.initialize(LocalConstants.YOUTUBE_API_KEY, onInitializedListener);
+                mPlayButton.setVisibility(View.GONE);
+            }
+        });
+
         return mView;
     }
 
@@ -73,6 +111,19 @@ public class One extends Fragment {
         mContext = context;
         mFragmentInterface = (FragmentBookInterface) mContext;
         mFragmentInterface.changeMenuItem(R.drawable.hamburguesa);
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser){
+        super.setUserVisibleHint(isVisibleToUser);
+        Log.d("Fragment", "Visible: " + isVisibleToUser);
+        if (isVisibleToUser) {
+            try {
+
+            } catch (Exception ea){
+                ea.printStackTrace();
+            }
+        }
     }
 
 }
